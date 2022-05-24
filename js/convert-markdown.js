e1d0ab179a70260c0b4fab41a25cb36132350e7d
Ian Finch <ianfinch@users.noreply.github.com>
2022-05-24 15:39:36 +0100

    Minor change to notes width

--- js/convert-markdown.js
+++ js/convert-markdown.js
@@ -1,7 +1,9 @@
 var converter = new showdown.Converter();
 converter.setOption("tables", true);
 
-var html = converter.makeHtml(document.getElementById("markdown").textContent)
+const md = document.getElementById("markdown").textContent;
+
+var html = converter.makeHtml(md)
             .replaceAll("<h2 ", "</section><h2 ")
             .replaceAll("<h2 ", "<section><h2 ");
 
