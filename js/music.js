f27d08dc30808ec042f5bc6710b1958458839c8e
Ian Finch <ianfinch@users.noreply.github.com>
2025-09-21 20:14:26 +0100

    Added a transpose dialogue

--- js/music.js
+++ js/music.js
@@ -24,21 +24,68 @@ addConfig = notation => {
 };
 
 /**
- * Do the conversion / rendering
+ * Calculate the steps needed to transpose to a new key
  */
-addEventListener("markdownConverted", () => {
+const stepsToTranspose = (newKey, visual) => {
+
+    const oldKey = visual[0].getKeySignature().root;
+    const steps = {
+        "C":  0,
+        "C#": 1,
+        "Db": 1,
+        "D":  2,
+        "D#": 3,
+        "Eb": 3,
+        "E":  4,
+        "F":  5,
+        "F#": 6,
+        "Gb": 6,
+        "G":  7,
+        "G#": 8,
+        "Ab": 8,
+        "A":  9,
+        "A#": 10,
+        "Bb": 10,
+        "B":  11
+    };
+
+    // Check for raising an octave
+    let octave = 0;
+    if (newKey.match(/'$/)) {
+
+        newKey = newKey.replace("'", "");
+        octave = 1;
+    }
+
+    return steps[newKey] - steps[oldKey] + 12 * octave;
+};
+
+/**
+ * Render the music
+ */
+const renderManuscript = transpose => {
 
     const notation = addConfig(getNotation());
     const visual = ABCJS.renderAbc("sheet", notation, { add_classes: true });
 
+    if (transpose) {
+
+        const transposed = ABCJS.strTranspose(notation, visual, stepsToTranspose(transpose, visual));
+        ABCJS.renderAbc("sheet", transposed, { add_classes: true })
+    }
+};
+
+/**
+ * Do the conversion / rendering
+ */
+addEventListener("markdownConverted", () => {
+
+    renderManuscript();
+
     // Give us a way to change the key signature
-    [...document.getElementsByClassName("abcjs-key-signature")].forEach(keySignature => {
+    document.getElementsByTagName("body")[0].addEventListener("click", () => {
 
-        keySignature.addEventListener("click", () => {
-
-            // TBD ... transposition is hard-coded, and isn't re-applied
-            const transposed = ABCJS.strTranspose(notation, visual, 5);
-            ABCJS.renderAbc("sheet", transposed, { add_classes: true })
-        });
+        const newKey = prompt("Enter the key to transpose to");
+        renderManuscript(newKey);
     });
 });
